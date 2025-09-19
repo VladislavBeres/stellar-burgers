406f46f3d85680d71782579c158bfc0850dedd5d
Vladislav Berestyuk <vadislavberestyuk@yandex.ru>
2025-09-19 23:17:56 +0300

    stellar-burgers TESTS 1.4

--- src/services/store/store.test.ts
+++ src/services/store/store.test.ts
@@ -1,19 +1,33 @@
-import store, { RootState } from './store';
+import store, { rootReducer, RootState } from './store';
 import { expect } from '@jest/globals';
+
+// импортируем редьюсеры каждого слайса
+import { constructorSlice } from '../slices/constructor/constructor';
+import { ingredientsSlise } from '../slices/ingredients/ingredients';
+import { feedsSlice } from '../slices/feed/feed';
+import { userSlice } from '../slices/user/user';
+import { userOrdersSlice } from '../slices/userOrders/userOrders';
+import { createOrderSlice } from '../slices/newOrder/newOrder';
 
 describe('rootReducer', () => {
   test('следует правильно инициализировать с помощью комбинированных редукторов', () => {
-    //проверка, что стор создан
+    // проверка, что стор создан
     expect(store).toBeDefined();
 
-    //получение состояния rootReducer
-    const state = store.getState() as RootState;
+    // создаём "левое" действие
+    const fakeAction = { type: 'UNKNOWN_ACTION' };
 
-    //проверка, что все слайсы существуют в rootReducer
-    expect(state.constructorIngredient).toBeDefined();
-    expect(state.ingredients).toBeDefined();
-    expect(state.feeds).toBeDefined();
-    expect(state.user).toBeDefined();
-    expect(state.newOrder).toBeDefined();
+    // инициализация rootReducer
+    const state = rootReducer(undefined, fakeAction) as RootState;
+
+    // проверяем, что начальное состояние совпадает с состоянием редьюсеров
+    expect(state).toEqual({
+      constructorIngredient: constructorSlice.reducer(undefined, fakeAction),
+      ingredients: ingredientsSlise.reducer(undefined, fakeAction),
+      feeds: feedsSlice.reducer(undefined, fakeAction),
+      user: userSlice.reducer(undefined, fakeAction),
+      userOrders: userOrdersSlice.reducer(undefined, fakeAction),
+      newOrder: createOrderSlice.reducer(undefined, fakeAction)
+    });
   });
 });
